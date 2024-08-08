d1556d506b0418b82285d1b579528293e1aba311
AmmarFK <farooqammar51@yahoo.com>
2024-08-08 21:24:45 +1000

    changes

--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -1,61 +1,62 @@
-const cardList = [
+const tasks = [
+  { title: "Buy groceries", details: "Milk, Bread, Eggs, and Fruits" },
+  { title: "Workout", details: "30 minutes of cardio and strength training" },
   {
-    title: "Kitten 2",
-    image: "images/kitten.jpeg",
-    link: "About Kitten 2",
-    desciption: "Demo desciption about kitten 2",
-  },
-  {
-    title: "Kitten 3",
-    image: "images/kitten.jpeg",
-    link: "About Kitten 3",
-    desciption: "Demo desciption about kitten 3",
+    title: "Finish project report",
+    details: "Complete the final report for the client project",
   },
 ];
 
-const clickMe = () => {
-  alert("Thanks for clicking me. Hope you have a nice day!");
-};
+function addTaskToTable(title, details) {
+  const tableBody = document.getElementById("task-table-body");
+  const row = `
+    <tr>
+      <td>${title}</td>
+      <td>${details}</td>
+    </tr>
+  `;
+  tableBody.innerHTML += row;
+}
 
 const submitForm = () => {
-  let formData = {};
-  formData.first_name = $("#first_name").val();
-  formData.last_name = $("#last_name").val();
-  formData.password = $("#password").val();
-  formData.email = $("#email").val();
-  console.log("Form Data Submitted: ", formData);
+  let temp = {};
+  temp.name = $("#task-name").val();
+  temp.details = $("#task-details").val();
+  tasks.push(temp);
+  temp.name != ""
+    ? addTaskToTable(temp.name, temp.details)
+    : console.log("No task name provided");
+  temp.name = "";
+  temp.details = "";
+  cancelTask();
+  console.log("Form Data Submitted: ", tasks);
 };
 
-const addCards = (items) => {
-  items.forEach((item) => {
-    let itemToAppend =
-      '<div class="col s4 center-align">' +
-      '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' +
-      item.image +
-      '">' +
-      '</div><div class="card-content">' +
-      '<span class="card-title activator grey-text text-darken-4">' +
-      item.title +
-      '<i class="material-icons right">more_vert</i></span><p><a href="#">' +
-      item.link +
-      "</a></p></div>" +
-      '<div class="card-reveal">' +
-      '<span class="card-title grey-text text-darken-4">' +
-      item.title +
-      '<i class="material-icons right">close</i></span>' +
-      '<p class="card-text">' +
-      item.desciption +
-      "</p>" +
-      "</div></div></div>";
-    $("#card-section").append(itemToAppend);
-  });
+const cancelTask = () => {
+  document.getElementById("task-name").value = "";
+  document.getElementById("task-details").value = "";
 };
 
 $(document).ready(function () {
-  $(".materialboxed").materialbox();
-  $("#formSubmit").click(() => {
+  const tableBody = document.getElementById("task-table-body");
+
+  tasks.forEach((task) => {
+    const row = `
+    <tr>
+      <td>${task.title}</td>
+      <td>${task.details}</td>
+    </tr>
+  `;
+    tableBody.innerHTML += row;
+  });
+
+  $("#save-task").click(() => {
     submitForm();
   });
-  addCards(cardList);
+
+  $("#cancel-task").click(() => {
+    cancelTask();
+  });
+
   $(".modal").modal();
 });
